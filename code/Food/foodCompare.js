a837b03b3e906c0638a4ec6da617e8b38f20a62e
deveun <deveun@github.com>
2019-10-21 14:31:56 +0900

    foodCompareResult 값 변경(수치비교), 차트 에러 수정

--- code/Food/foodCompare.js
+++ code/Food/foodCompare.js
@@ -1,4 +1,14 @@
-module.exports.function = function foodCompare (food, foodTwo) {
-  var res = "비교결과값";
+module.exports.function = function foodCompare(food, foodTwo) {
+  //각 성분을 비교하고, 1번이 더 큰경우 -1 // 비교가 불가능 (같거나 Integer가 아니면)이면 0 // 2번이 더 큰 경우 1
+  var nutriEng = ["calorie", "carbo", "fat", "natrium", "sugar", "protein"];
+  var res = new Array;
+  for (var i = 0; i < 6; i++) {
+    if (food[nutriEng[i]] > foodTwo[nutriEng[i]])
+      res.push(-1);
+    else if (food[nutriEng[i]] < foodTwo[nutriEng[i]])
+      res.push(1);
+    else
+      res.push(0);
+  }
   return res;
 }
