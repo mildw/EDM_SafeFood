ad071f863219bc94f465b59aab1ba3f415c51758
minsu <mildup.kim@gail.com>
2019-10-22 01:47:33 +0900

    트레이닝조금추가

--- code/Restaurant/restaurantSearch.js
+++ code/Restaurant/restaurantSearch.js
@@ -2,12 +2,17 @@ module.exports.function = function restaurantSearch (dummy,myPoint) {
  var console = require('console');
   var http = require('http');
   var searchResult = [];
-  var url = 'http://54.180.149.204/restaurant/'; //이 주소 뺴놓으면 될듯
+  var url = 'http://54.180.149.204/restaurant/searchRange.php'; //이 주소 뺴놓으면 될듯
   var sp = myPoint;
-  var queryParams = 'searchRange.php';
-  queryParams += '?' + 'lat='+ sp.point.latitude;
-  queryParams += '&'+ 'lng='+sp.point.longitude;
-  searchResult = http.getUrl(url+queryParams,
+  var param = {
+    servername:"localhost",
+    username:"root",
+    password:"123300",
+    dbname:"bixby",
+    lat : sp.point.latitude,
+    lng : sp.point.longitude
+  }
+  searchResult = http.postUrl(url, param,
     { format: 'json' });
   return searchResult;
 }
