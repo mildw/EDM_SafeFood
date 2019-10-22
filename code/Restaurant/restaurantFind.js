4c566786867830b94659549258fb20d2a89ff5d6
danidanee <ssongda9412@naver.com>
2019-10-22 12:36:11 +0900

    레이아웃 80프로 완성, C Driver 수정중

--- code/Restaurant/restaurantFind.js
+++ code/Restaurant/restaurantFind.js
@@ -2,24 +2,28 @@ module.exports.function = function restaurantFind(restaurantLocation, restaurant
   var console = require('console');
   var http = require('http');
   var result = [];
-  var url = 'http://54.180.149.204/restaurant/'; //이 주소 뺴놓으면 될듯
-  var queryParams = 'selectRestaurant.php';
-  //queryParams += '?' + 'name='+restaurantName;
-  //queryParams += '&'+'location='+restaurantLocation;
+  var url = 'http://54.180.149.204/restaurant/selectRestaurant.php'; //이 주소 뺴놓으면 될듯
+  var queryParams = '';
   queryParams += '?'+'location='+restaurantLocation;
-
-  result = http.getUrl(url + queryParams,
+  var param={
+    servername:"localhost",
+    username:"root",
+    password:"123300",
+    dbname:"bixby",
+    location : restaurantLocation
+  }
+  result = http.postUrl(url ,param,
     { format: 'json' });
   console.log(result);
-
+​
   
   if (restaurantName) {
     result = result.filter(function (restaurant) {
       return restaurant.name.includes(restaurantName);
     });
   }
-
+​
   console.log(result);
-
+​
   return result;
 }
