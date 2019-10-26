8478a3aac852856d27bf973c130950990610c145
mildup <mildup.kim@gmail.com>
2019-10-26 20:46:04 +0900

    delete func_drop

--- code/Restaurant/restaurantFind.js
+++ code/Restaurant/restaurantFind.js
@@ -4,30 +4,27 @@ module.exports.function = function restaurantFind(restaurantLocation, restaurant
   var fail = require('fail');
   try {
     var result = [];
+    var copy = [];
     var url = 'http://54.180.149.204/restaurant/selectRestaurant.php'; //이 주소 뺴놓으면 될듯
-    var queryParams = '';
-    queryParams += '?' + 'location=' + restaurantLocation;
-    var param = {
-      servername: "localhost",
-      username: "root",
-      password: "123300",
-      dbname: "bixby",
-      location: restaurantLocation
-    }
-    result = http.postUrl(url, param,
+    var queryParams = '?' + 'location=' + restaurantLocation;
+
+    result = http.getUrl(url + queryParams,
       { format: 'json' });
     console.log(result);
 
 
     if (restaurantName) {
-      result = result.filter(function (restaurant) {
+      copy = result.filter(function (restaurant) {
         return restaurant.name.includes(restaurantName);
       });
     }
 
-    console.log(result);
+    if (copy.length == 0) {
+      return result;
+    }else{
+      return copy;
+    }
 
-    return result;
   } catch (e) {
     throw fail.checkedError('', 'Error', {});
   }
--- code/Restaurant/restaurantSearch.js
+++ code/Restaurant/restaurantSearch.js
@@ -7,15 +7,11 @@ module.exports.function = function restaurantSearch (myPoint) {
     var searchResult = [];
     var url = 'http://54.180.149.204/restaurant/searchRange.php'; //이 주소 뺴놓으면 될듯
     var sp = myPoint;
-    var param = {
-      servername: "localhost",
-      username: "root",
-      password: "123300",
-      dbname: "bixby",
-      lat: sp.point.latitude,
-      lng: sp.point.longitude
-    }
-    searchResult = http.postUrl(url, param,
+    var queryParams = '?' + 'lat='+ sp.point.latitude;
+    queryParams += '&'+ 'lng='+sp.point.longitude;
+    
+    
+    searchResult = http.getUrl(url+queryParams,
       { format: 'json' });
     return searchResult;
   } catch (e) {
