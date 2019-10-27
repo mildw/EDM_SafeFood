f0bc1da9cfea8b9f8b3c81def820f59e04d1ec19
danidanee <ssongda9412@naver.com>
2019-10-27 18:36:22 +0900

    레이아웃 통계 수정

--- code/Allergy/findAllergy.js
+++ code/Allergy/findAllergy.js
@@ -1,4 +1,4 @@
-module.exports.function = function findAllergy(findAllergyItem) {
+module.exports.function = function findAllergy(foodName) {
 
   var http = require('http')
   var console = require('console')
@@ -9,21 +9,10 @@ module.exports.function = function findAllergy(findAllergyItem) {
   try {
 
     var param = "?" + encodeURIComponent('ServiceKey') + '=' + secret.get('foodkey');
-    param += "&" + encodeURIComponent('prdlstNm') + '=' + encodeURIComponent(findAllergyItem);
+    param += "&" + encodeURIComponent('prdlstNm') + '=' + encodeURIComponent(foodName);
     param += "&returnType=json";
     var findAllergyResults = http.getUrl(config.get('foodUrl') + param, { format: 'json' });
     var findAllergyResultTmp = findAllergyResults.list;
-
-    //검색된 값이 없으면 _ 띄어쓰기 없애서 한번 더 해주기
-    if (findAllergyResultTmp.length == 0) {
-
-      var findAllergyItemTmp = findAllergyItem.replace(" ", "");
-      param = "?" + encodeURIComponent('ServiceKey') + '=' + secret.get('foodkey');
-      param += "&" + encodeURIComponent('prdlstNm') + '=' + encodeURIComponent(findAllergyItemTmp);
-      param += "&returnType=json";
-      findAllergyResults = http.getUrl(config.get('foodUrl') + param, { format: 'json' });
-      findAllergyResultTmp = findAllergyResults.list;
-    }
 
     var findAllergyResult = [];
     var name_length = 10000;
@@ -38,7 +27,8 @@ module.exports.function = function findAllergy(findAllergyItem) {
       }
     }
     return findAllergyResult[0];
-  } catch (e) {
+  }
+   catch (e) {
     throw fail.checkedError('', 'Error', {});
   }
 }
@@ -48,36 +38,34 @@ function Find(findAllergyResultTmp) {
   //나중에 url 이미지 리스트 넣어주기
   var allergy_urllist = ["Aimg/A0.jpg", "Aimg/A1.jpg", "Aimg/A2.jpg", "Aimg/A3.jpg", "Aimg/A4.jpg", "Aimg/A5.jpg", "Aimg/A6.jpg", "Aimg/A7.jpg", "Aimg/A8.jpg", "Aimg/A9.jpg", "Aimg/A10.jpg", "Aimg/A11.jpg", "Aimg/A12.jpg", "Aimg/A13.jpg", "Aimg/A14.jpg", "Aimg/A15.png"];
   //초기에는 다 없는 걸로 설정
-  var allergy = ["Aimg/NoA0.png", "Aimg/NoA1.png", "Aimg/NoA2.png", "Aimg/NoA3.png", "Aimg/NoA4.png", "Aimg/NoA5.png", "Aimg/NoA6.png", "Aimg/NoA7.png", "Aimg/NoA8.png", "Aimg/NoA9.png", "Aimg/NoA10.png", "Aimg/NoA11.png", "Aimg/NoA12.png", "Aimg/NoA13.png", "Aimg/NoA14.png", "Aimg/NoA15.png"];
 
   var image_al = [];
   var cnt = 0;
   var start = 0;
   //추가로 난류
   if (findAllergyResultTmp.allergy.match('메추리알') == '메추리알') {
-    allergy[0] = allergy_urllist[0];
+    //allergy[0] = allergy_urllist[0];
     image_al[cnt++] = { url: allergy_urllist[0] };
     start++;
   }
   else if (findAllergyResultTmp.allergy.match('난류') == '난류') {
-    allergy[0] = allergy_urllist[0];
+    //allergy[0] = allergy_urllist[0];
     image_al[cnt++] = { url: allergy_urllist[0] };
     start++;
   }
   //알러지 체크해주기
   for (var i = start; i < allergy_list.length; i++) {
     if (findAllergyResultTmp.allergy.match(allergy_list[i]) == allergy_list[i]) {
-      allergy[i] = allergy_urllist[i];
+      //allergy[i] = allergy_urllist[i];
       image_al[cnt++] = { url: allergy_urllist[i] };
     }
   }
+
   var findAllergyResult = {
     prdlstNm: findAllergyResultTmp.prdlstNm,
-    imgurl1: findAllergyResultTmp.imgurl1,
-    imgurl2: findAllergyResultTmp.imgurl2,
     //알러지체크
     allergy: findAllergyResultTmp.allergy,
-    allergy_set: allergy,
+    //allergy_set: allergy,
     images: [{ url: findAllergyResultTmp.imgurl1 }, { url: findAllergyResultTmp.imgurl2 }],
     aimages: image_al
   };
